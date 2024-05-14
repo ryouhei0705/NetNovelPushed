d1c8c7476b5df0f78e5dffa915ae7854008876f3
ryouhei <@ryouhei_0705>
2024-05-14 21:48:48 +0900

    Initial Commit

---
+++ NetNovelPushed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetNovelPushed
+//
+//  Created by 清水瞭平 on 2024/05/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetNovelPushed/NetNovelPushedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetNovelPushedApp.swift
+//  NetNovelPushed
+//
+//  Created by 清水瞭平 on 2024/05/14.
+//
+
+import SwiftUI
+
+@main
+struct NetNovelPushedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
